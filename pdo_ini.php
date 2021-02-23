303d3e9928eb113b67d409438da3004e3aa7ea72
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-02-23 15:41:58 +0200

    added conection to db and tables.php

---
+++ pdo_ini.php
@@ -0,0 +1,12 @@
+<?php
+$config = require_once './config.php';
+
+try {
+    $pdo = new PDO(
+        sprintf('mysql:host=%s;dbname=%s', $config['host'], $config['dbname']),
+        $config['user'],
+        $config['pass']
+    );
+    } catch (PDOException $exception) {
+        echo $exception->getMessage();
+}
