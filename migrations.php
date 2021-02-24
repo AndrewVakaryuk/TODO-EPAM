b4617f438b26d143a2cd78678e09a44cb454e9d2
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-02-24 17:54:15 +0200

    last updates

--- migrations.php
+++ migrations.php
@@ -20,23 +20,11 @@ CREATE TABLE `todo_tasks` (
 	`is_done` TINYINT(1) NOT NULL,
 	`title` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
 	`created_at` DATETIME NOT NULL DEFAULT current_timestamp(),
---	`user_id` INT(10) UNSIGNED,
 	PRIMARY KEY (`id`),
  	FOREIGN KEY (`todo_list_id`) REFERENCES todo_lists(id) ON DELETE CASCADE
 );
 SQL;
 $pdo->exec($sql);
 
-//$sql = <<<'SQL'
-//DROP TABLE IF EXISTS `users`;
-//CREATE TABLE `users` (
-//	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-//	`username` VARCHAR(25) NOT NULL COLLATE 'utf8_general_ci',
-//	`password` VARCHAR(25) NOT NULL COLLATE 'utf8_general_ci',
-//	`email` VARCHAR(25) NOT NULL COLLATE 'utf8_general_ci',
-//	PRIMARY KEY (`id`)
-//);
-//SQL;
-//$pdo->exec($sql);
 
 
